c8db1093504cd896fa619410d27de942f3633c3f
#MWHT <mwht@wp.pl>
2018-02-27 17:10:34 +0100

    rename MainClass to QuadraticEquation

---
+++ QuadraticEquation.java
@@ -0,0 +1,17 @@
+public class MainClass {
+  private void printUsage() {
+    System.out.println(
+    "usage: java MainClass <a> <b> <c>");
+    System.out.println(
+    "\ta - a part of equation");
+    System.out.println(
+    "\tb - b part of equation");
+    System.out.println(
+    "\tc - c part of equation");    
+  }
+  public static void main(String[] args) {
+    if(args.length == 0) {
+
+    }
+  }
+}
