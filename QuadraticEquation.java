691ef054ac69e7bac29c9e0aceeb1a2d24fe573b
#MWHT <mwht@wp.pl>
2018-02-27 16:51:06 +0100

    add QuadraticEquation class

---
+++ QuadraticEquation.java
@@ -0,0 +1,8 @@
+public class QuadraticEquation {
+  private double a;     // "a" part of equation
+  private double b;     // "b" part of equation
+  private double c;     // "c" part of equation
+  private double delta;
+  private double root1;
+  private double root2;
+}
