b7018e26621bdbd0e31cdab7735031dfb91ee9e8
#MWHT <mwht@wp.pl>
2018-02-27 16:52:46 +0100

    add constructor for QuadraticEquation

--- QuadraticEquation.java
+++ QuadraticEquation.java
@@ -2,7 +2,16 @@ public class QuadraticEquation {
   private double a;     // "a" part of equation
   private double b;     // "b" part of equation
   private double c;     // "c" part of equation
-  private double delta;
-  private double root1;
-  private double root2;
+  private double delta; // delta calculated from equation
+  private double root1; // first root from equation
+  private double root2; // second root from equation
+
+  public QuadraticEquation() {
+    a = 0;
+    b = 0;
+    c = 0;
+    delta = 0;
+    root1 = 0;
+    root2 = 0;
+  }
 }
